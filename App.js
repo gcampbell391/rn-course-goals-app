fd9f8a31b35fbfde1a77f991903801e44db29c76
gcampbell391 <gcampbell391@gmail.com>
2020-07-21 20:21:44 -0400

    styling and error handling added

--- App.js
+++ App.js
@@ -15,6 +15,10 @@ export default function App() {
   }
 
   const addGoal = () => {
+    if (enteredGoal.length === 0) {
+      alert('Please enter a goal')
+      return
+    }
     setCourseGoals(currentGoals => [...currentGoals, { key: Math.random().toString(), value: enteredGoal }])
     setShowInputModal(false)
     setEnteredGoal('')
@@ -29,7 +33,10 @@ export default function App() {
 
   return (
     <View style={styles.screen}>
-      <Text style={styles.header}>Goal Conqueror</Text>
+      <Text style={styles.header}>Goal Getter</Text>
+      <Text style={styles.about}>Add a goal by clicking the button below</Text>
+      <Text style={styles.about}>Delete a goal by clicking said goal</Text>
+      <Text style={styles.about}>Created by GC3</Text>
       <Button title='Add New Goal' onPress={() => setShowInputModal(true)} />
       <GoalInput enteredGoal={enteredGoal} addGoal={addGoal} goalInputHandler={goalInputHandler} showInput={showInputModal} cancelAdd={() => setShowInputModal(false)} />
       <FlatList data={courseGoals} renderItem={itemData => <GoalItem id={itemData.item.key} title={itemData.item.value} onDelete={removeGoal} />} />
@@ -43,12 +50,23 @@ const styles = StyleSheet.create({
     padding: 50,
     backgroundColor: 'yellow',
     height: '100%'
+
   },
   header: {
-    fontSize: 35,
+    fontSize: 50,
     textAlign: 'center',
     marginBottom: 30,
     color: '#80d8ff',
-    fontWeight: 'bold'
+    fontWeight: 'bold',
+    textShadowColor: 'rgba(0, 0, 0, 0.75)',
+    textShadowOffset: { width: -1, height: 1 },
+    textShadowRadius: 10
+  },
+  about: {
+    fontSize: 13,
+    textAlign: 'center',
+    fontWeight: 'bold',
+    marginVertical: 5,
+
   }
 });
--- components/GoalInput.js
+++ components/GoalInput.js
@@ -1,13 +1,16 @@
 import React from 'react'
-import { View, TextInput, Button, StyleSheet, Modal } from 'react-native'
+import { View, TextInput, Button, StyleSheet, Modal, Text } from 'react-native'
 
 const GoalInput = (props) => {
     return (
         <Modal visible={props.showInput} animationType='slide'>
             <View style={styles.inputContainer}>
-                <TextInput placeholder="Enter Course Goal" style={styles.input} onChangeText={props.goalInputHandler} value={props.enteredGoal} />
-                <Button title="Add" onPress={props.addGoal} />
-                <Button title="Cancel" color="red" onPress={props.cancelAdd} />
+                <Text style={styles.inputHeader}>What are you trying to accomplish?</Text>
+                <TextInput placeholder="Enter Goal" style={styles.input} onChangeText={props.goalInputHandler} value={props.enteredGoal} />
+                <View style={styles.btnContainer}>
+                    <Button title="Add" onPress={props.addGoal} />
+                    <Button title="Cancel" color="red" onPress={props.cancelAdd} />
+                </View>
             </View>
         </Modal>
     )
@@ -27,6 +30,21 @@ const styles = StyleSheet.create({
         width: '70%',
         marginBottom: 10,
         backgroundColor: 'white'
+    },
+    inputHeader: {
+        fontSize: 40,
+        textAlign: 'center',
+        marginBottom: 50,
+        color: '#80d8ff',
+        fontWeight: 'bold',
+        textShadowColor: 'rgba(0, 0, 0, 0.75)',
+        textShadowOffset: { width: -1, height: 1 },
+        textShadowRadius: 10
+    },
+    btnContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        width: '50%'
     }
 })
 
